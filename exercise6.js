3974b7031f1e72d4f6a6a7e394b1c64a5953c4ff
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-02-13 00:12:03 -0800

    Update exercise6.js

--- exercise6.js
+++ exercise6.js
@@ -3,22 +3,41 @@
 [2] === [2] 
 {} === {} 
 //Sol'n
-//Both above are non-primitive types so they are unique and not treated as equals
+//Both above are non-primitive types so they are unique and not treated as equals, false
 
 
 //#2 what is the value of property a for each object.
 const object1 = { a: 5 }; 
-//Ans: 5
+//Ans: 4
 const object2 = object1; 
-//Ans: 5
+//Ans: 4
 const object3 = object2; 
-//Ans: 5
+//Ans: 4
 const object4 = { a: 5}; 
-//Ans: 5
+//Ans: 4
 object1.a = 4;
 //Ans: 5
 
 
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+
+class Animal {
+	constructor(name, type, color) {
+		this.name = name;
+		this.color = color;
+		this.type = type;
+	}
+}
+
+class Mamal extends Animal {
+	constructor(name, type, color) {
+		super(name, type, color)
+	}
+	sound() {
+		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
+	}
+}
+
+const cow = new Mamal('Shelly', 'cow', 'brown');
 
